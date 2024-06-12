cf676c2f4f8e49ba5e72e95aeeccc8dc7bc3c5a3
Becafig <lc.becafig@gmail.com>
2024-06-12 15:53:11 -0300

    Update scroll.js

--- scroll.js
+++ scroll.js
@@ -3,9 +3,9 @@ let prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
   let currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.backgroundColor = "rgba(0, 0, 0)"; // cor da barra fixa
+    document.getElementById("navbar").style.backgroundColor = "#000137"; // cor da barra fixa
   } else {
-    document.getElementById("navbar").style.backgroundColor = "rgba(0, 0, 0, 0.3)"; // cor/transparencia da barra fixa ao ser rolado a pagina para baixo
+    document.getElementById("navbar").style.backgroundColor = "rgba(0, 0, 0, 0.4)"; // cor/transparencia da barra fixa ao ser rolado a pagina para baixo
   }
   prevScrollpos = currentScrollPos;
 }
